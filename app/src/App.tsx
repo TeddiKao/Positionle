eee2d1d3afd68b84775e9d4d249923c189315f93
Teddi <teddikao@gmail.com>
2026-01-24 15:28:52 +0800

    Remove BrowserRouter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router"
+import { Route, Routes } from "react-router";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<h1>Home Page</h1>} />
-			</Routes>
-		</BrowserRouter>
-	)
+		<Routes>
+			<Route path="/" element={<h1>Home Page</h1>} />
+		</Routes>
+	);
 }
 
-export default App
+export default App;
