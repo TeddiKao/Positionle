dcec0d6c354b1063d1fd44fdc5898d48e805f8df
Teddi <teddikao@gmail.com>
2026-01-25 18:16:43 +0800

    Add className prop to LeftArrow.tsx for flexible styling

--- app/src/features/gameplay/icons/guessNavigator/LeftArrow.tsx
+++ app/src/features/gameplay/icons/guessNavigator/LeftArrow.tsx
@@ -1,5 +1,13 @@
-function LeftArrow() {
+import {IconChevronLeft} from "@tabler/icons-react";
 
+type LeftArrowProps = {
+	className?: string;
+}
+
+function LeftArrow({ className }: LeftArrowProps) {
+	return (
+		<IconChevronLeft className={className} />
+	)
 }
 
 export default LeftArrow;
