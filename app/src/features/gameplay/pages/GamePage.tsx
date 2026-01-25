99a4195497d68865264de4e2ea9dcca57f26e7b0
Teddi <teddikao@gmail.com>
2026-01-25 16:10:59 +0800

    Create header with logo

--- app/src/features/gameplay/pages/GamePage.tsx
+++ app/src/features/gameplay/pages/GamePage.tsx
@@ -1,7 +1,16 @@
+import PawnLogo from "../icons/logo/PawnLogo.tsx";
+
 function GamePage() {
 	return (
 		<div className="flex flex-col items-center">
-			<h1 className="text-3xl font-bold">Positionle</h1>
+			<span className="flex flex-row items-end text-3xl font-bold mt-2 font-logo">
+				{/* Pawn logo is used in place of the letter "i" */}
+				<span>Pos</span>
+				<PawnLogo className="inline-flex w-8 h-8 -ml-1" />
+				<span className="-ml-1">t</span>
+				<PawnLogo className="inline-flex w-8 h-8 -ml-1" />
+				<span className="-ml-1">onle</span>
+			</span>
 		</div>
 	)
 }
