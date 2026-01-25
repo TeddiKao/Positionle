409cdd29cc648828df1ef2eceddfe5acc12296e9
Teddi <teddikao@gmail.com>
2026-01-25 15:47:43 +0800

    Create heading for game page

--- app/src/features/gameplay/pages/GamePage.tsx
+++ app/src/features/gameplay/pages/GamePage.tsx
@@ -1,5 +1,9 @@
 function GamePage() {
-
+	return (
+		<div className="flex flex-col items-center">
+			<h1 className="text-3xl font-bold">Positionle</h1>
+		</div>
+	)
 }
 
 export default GamePage;
