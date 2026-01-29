65072712756b9a644408bdd7cf559e70ee978466
Teddi <teddikao@gmail.com>
2026-01-29 17:59:55 +0800

    Add constants for piece and color abbreivations

---
+++ app/src/features/gameplay/constants/abbrieviations.ts
@@ -0,0 +1,15 @@
+const colorAbbreviations = {
+	white: "w",
+	black: "b",
+}
+
+const pieceAbbreviations = {
+	king: "K",
+	queen: "Q",
+	rook: "R",
+	knight: "N",
+	bishop: "B",
+	pawn: "P"
+}
+
+export { colorAbbreviations, pieceAbbreviations }
