d3c975afc21f59c9d7e7339857ad6cd4535fc847
Teddi <teddikao@gmail.com>
2026-01-29 11:36:45 +0800

    Add flex styling to actions menu

--- app/src/features/gameplay/pages/GamePage.tsx
+++ app/src/features/gameplay/pages/GamePage.tsx
@@ -54,6 +54,10 @@ function GamePage() {
 						})
 					))}
 				</div>
+
+				<div className="flex flex-col items-center justify-center">
+
+				</div>
 			</div>
 
 			<Button type="button" className="w-full max-w-md hover:opacity-90">Check</Button>
