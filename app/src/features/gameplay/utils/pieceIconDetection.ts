68897badac81220de19292c1e6caddfec51713dc
Teddi <teddikao@gmail.com>
2026-01-29 21:14:08 +0800

    Split icon constants into two objects

--- app/src/features/gameplay/utils/pieceIconDetection.ts
+++ app/src/features/gameplay/utils/pieceIconDetection.ts
@@ -1,13 +1,15 @@
 import {colorAbbreviations, pieceAbbreviations} from "@/features/gameplay/constants/abbrieviations";
-import {pieceIcons} from "@/features/gameplay/constants/pieceIcons";
+import {whitePieceIcons, blackPieceIcons} from "@/features/gameplay/constants/pieceIcons";
 
 function getPieceIcon(color: "white" | "black", piece: "queen" | "rook" | "knight" | "bishop" | "king" | "pawn") {
 	const pieceAbbreviation = pieceAbbreviations[piece];
 	const colorAbbreviation = colorAbbreviations[color];
 
-	const iconName: "bK" | "bN" | "bB" | "bR" | "bQ" | "bP" | "wK" | "wN" | "wB" | "wR" | "wQ" | "wP" = `${colorAbbreviation}${pieceAbbreviation}`;
-
-	return pieceIcons[iconName];
+	if (colorAbbreviation === "w") {
+		return whitePieceIcons[`${colorAbbreviation}${pieceAbbreviation}`];
+	} else {
+		return blackPieceIcons[`${colorAbbreviation}${pieceAbbreviation}`];
+	}
 }
 
 export { getPieceIcon };
