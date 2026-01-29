7cef119d277b908ba3621bcfa10a2b0c2be895cb
Teddi <teddikao@gmail.com>
2026-01-29 21:29:18 +0800

    Fix issue with icon imports

--- app/src/features/gameplay/constants/pieceIcons.ts
+++ app/src/features/gameplay/constants/pieceIcons.ts
@@ -1,13 +1,13 @@
 import bK from "../icons/chess/bK.svg";
 import bN from "../icons/chess/bN.svg"
-import bP from "../icons/chess/bK.svg";
+import bP from "../icons/chess/bP.svg";
 import bQ from "../icons/chess/bQ.svg";
 import bB from "../icons/chess/bB.svg";
 import bR from "../icons/chess/bR.svg";
 
 import wK from "../icons/chess/wK.svg";
 import wN from "../icons/chess/wN.svg"
-import wP from "../icons/chess/wK.svg";
+import wP from "../icons/chess/wP.svg";
 import wQ from "../icons/chess/wQ.svg";
 import wB from "../icons/chess/wB.svg";
 import wR from "../icons/chess/wR.svg";
