3800da0e0ddf1bb805026838cd44b0744d5cf5f8
Teddi <teddikao@gmail.com>
2026-01-29 20:58:05 +0800

    Add types to abbreviation mappings

--- app/src/features/gameplay/constants/abbrieviations.ts
+++ app/src/features/gameplay/constants/abbrieviations.ts
@@ -1,9 +1,23 @@
-const colorAbbreviations = {
+type ColorAbbreviations = {
 	white: "w",
 	black: "b",
 }
 
-const pieceAbbreviations = {
+type PieceAbbreviations = {
+	king: "K",
+	queen: "Q",
+	rook: "R",
+	knight: "N",
+	bishop: "B",
+	pawn: "P",
+}
+
+const colorAbbreviations: ColorAbbreviations = {
+	white: "w",
+	black: "b",
+}
+
+const pieceAbbreviations: PieceAbbreviations = {
 	king: "K",
 	queen: "Q",
 	rook: "R",
