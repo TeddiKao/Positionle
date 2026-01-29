d50c1b53010feb8b1eca06c3af48ed38887a280a
Teddi <teddikao@gmail.com>
2026-01-29 16:52:58 +0800

    Create component for Eraser icon

--- app/src/features/gameplay/icons/actionMenu/Eraser.tsx
+++ app/src/features/gameplay/icons/actionMenu/Eraser.tsx
@@ -1,5 +1,13 @@
-function Eraser() {
+import {IconEraser} from "@tabler/icons-react";
 
+type EraserIconProps = {
+	className?: string;
+}
+
+function Eraser({ className = "" }: EraserIconProps) {
+	return (
+		<IconEraser className={className} />
+	)
 }
 
 export default Eraser;
