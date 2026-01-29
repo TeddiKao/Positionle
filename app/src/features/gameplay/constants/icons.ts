96da8d6bccc8ed088b6572b65b4bec9dce1762b9
Teddi <teddikao@gmail.com>
2026-01-29 20:51:27 +0800

    Create constants for chess piece icons

---
+++ app/src/features/gameplay/constants/icons.ts
@@ -0,0 +1,31 @@
+import bK from "../icons/chess/bK.svg";
+import bN from "../icons/chess/bN.svg"
+import bP from "../icons/chess/bK.svg";
+import bQ from "../icons/chess/bQ.svg";
+import bB from "../icons/chess/bB.svg";
+import bR from "../icons/chess/bR.svg";
+
+import wK from "../icons/chess/wK.svg";
+import wN from "../icons/chess/wN.svg"
+import wP from "../icons/chess/wK.svg";
+import wQ from "../icons/chess/wQ.svg";
+import wB from "../icons/chess/wB.svg";
+import wR from "../icons/chess/wR.svg";
+
+const icons = {
+	bK,
+	bN,
+	bQ,
+	bB,
+	bR,
+	bP,
+
+	wK,
+	wN,
+	wQ,
+	wB,
+	wR,
+	wP
+}
+
+export { icons }
