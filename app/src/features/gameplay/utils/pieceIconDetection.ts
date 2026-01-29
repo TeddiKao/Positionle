2e488687745099921288d9182aa90a511787f7bd
Teddi <teddikao@gmail.com>
2026-01-29 21:03:04 +0800

    Create utility function to get piece icon

--- app/src/features/gameplay/utils/pieceIconDetection.ts
+++ app/src/features/gameplay/utils/pieceIconDetection.ts
@@ -1,5 +1,13 @@
-function getPieceIcon(color: "white" | "black", piece: "queen" | "rook" | "knight" | "bishop" | "king") {
+import {colorAbbreviations, pieceAbbreviations} from "@/features/gameplay/constants/abbrieviations";
+import {pieceIcons} from "@/features/gameplay/constants/pieceIcons";
 
+function getPieceIcon(color: "white" | "black", piece: "queen" | "rook" | "knight" | "bishop" | "king" | "pawn") {
+	const pieceAbbreviation = pieceAbbreviations[piece];
+	const colorAbbreviation = colorAbbreviations[color];
+
+	const iconName: "bK" | "bN" | "bB" | "bR" | "bQ" | "bP" | "wK" | "wN" | "wB" | "wR" | "wQ" | "wP" = `${colorAbbreviation}${pieceAbbreviation}`;
+
+	return pieceIcons[iconName];
 }
 
 export { getPieceIcon };
