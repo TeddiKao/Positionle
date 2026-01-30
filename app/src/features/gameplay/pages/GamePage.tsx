96cc24a7c52d79c2844871282db7e6c82a6044c6
Teddi <teddikao@gmail.com>
2026-01-30 12:07:18 +0800

    Round corners of piece menu buttons

--- app/src/features/gameplay/pages/GamePage.tsx
+++ app/src/features/gameplay/pages/GamePage.tsx
@@ -40,7 +40,7 @@ function GamePage() {
 					<div className="flex flex-row w-max p-1 rounded-md shadow-md shadow-gray-400">
 						<div className="flex flex-col">
 							{Object.entries(whitePieceIcons).map(([abbreviation, icon]) => (
-								<button key={abbreviation} type="button" className="hover:bg-gray-400">
+								<button key={abbreviation} type="button" className="hover:bg-gray-400 rounded-md">
 									<img className="w-12 h-12" src={icon} alt={abbreviation} />
 								</button>
 							))}
@@ -48,7 +48,7 @@ function GamePage() {
 
 						<div className="flex flex-col">
 							{Object.entries(blackPieceIcons).map(([abbreviation, icon]) => (
-								<button type="button" key={abbreviation} className="hover:bg-gray-400">
+								<button type="button" key={abbreviation} className="hover:bg-gray-400 rounded-md">
 									<img className="w-12 h-12" src={icon} alt={abbreviation} />
 								</button>
 							))}
