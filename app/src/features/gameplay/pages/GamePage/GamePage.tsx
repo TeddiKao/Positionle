f4a3e6216e6d4b0490f8966d5fe6159dff791d4d
Teddi <teddikao@gmail.com>
2026-01-30 20:39:48 +0800

    Remove all unused imports and constants

--- app/src/features/gameplay/pages/GamePage/GamePage.tsx
+++ app/src/features/gameplay/pages/GamePage/GamePage.tsx
@@ -1,12 +1,4 @@
-import PawnLogo from "../../icons/logo/PawnLogo";
-import LeftArrow from "../../icons/guessNavigator/LeftArrow";
-import RightArrow from "../../icons/guessNavigator/RightArrow";
-import {isSquareOnBottomEdge, isSquareOnLeftEdge} from "../../utils/edgeDetection";
 import {Button} from "@/components/ui/button";
-import Eraser from "@/features/gameplay/icons/actionMenu/Eraser";
-import TrashCan from "@/features/gameplay/icons/actionMenu/TrashCan";
-import Flip from "@/features/gameplay/icons/actionMenu/Flip";
-import {blackPieceIcons, whitePieceIcons} from "@/features/gameplay/constants/pieceIcons";
 import Logo from "@/features/gameplay/pages/GamePage/components/Logo";
 import GuessNavigator from "@/features/gameplay/pages/GamePage/components/GuessNavigator";
 import PieceSetupMenu from "@/features/gameplay/pages/GamePage/components/PieceSetupMenu";
@@ -14,10 +6,7 @@ import ChessboardGrid from "@/features/gameplay/pages/GamePage/components/Chessb
 import ActionsMenu from "@/features/gameplay/pages/GamePage/components/ActionsMenu";
 
 function GamePage() {
-	const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
-	const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
-
-	return (
+		return (
 		<div className="flex flex-col items-center gap-4">
 			<Logo />
 			<GuessNavigator />
