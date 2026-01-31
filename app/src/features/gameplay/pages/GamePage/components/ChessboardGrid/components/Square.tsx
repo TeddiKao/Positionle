3587ec3926ab9c686d3319318d986e3416eb6443
Teddi <teddikao@gmail.com>
2026-01-31 17:01:00 +0800

    Extract additional logic into square component

--- app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square.tsx
+++ app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square.tsx
@@ -1,26 +1,57 @@
-import type {ChessPiece, PieceColor} from "@/features/gameplay/types/chess";
-import type {SquareCoordinate} from "@/features/gameplay/types/coordinates";
 import {getPieceIcon} from "@/features/gameplay/utils/pieceIconDetection";
-import {useDroppable} from "@dnd-kit/core";
+import {DndContext, useDroppable} from "@dnd-kit/core";
+import {isSquareOnBottomEdge, isSquareOnLeftEdge} from "@/features/gameplay/utils/edgeDetection";
+import useGuessesStore from "@/features/gameplay/stores/guesses";
 
 type SquareProps = {
-	color: PieceColor,
-	piece: ChessPiece
-	coordinate: SquareCoordinate
+	file: "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h",
+	rank: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 }
 
-function Square({color, piece, coordinate}: SquareProps) {
+function Square({file, rank}: SquareProps) {
 	const {setNodeRef} = useDroppable({
-		id: coordinate
+		id: `${file}${rank}`
 	});
 
-	const pieceIcon = getPieceIcon(color, piece);
+	const { guesses, currentGuess } = useGuessesStore();
+
+	const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+	const fileIndex = files.indexOf(file);
+	const isDark = (rank + fileIndex) % 2 === 1;
 
 	return (
-		<div ref={setNodeRef}>
-			<img key={`${coordinate} ${color} ${piece}`} src={pieceIcon}
-								   alt={`${color} ${piece}`}/>
-		</div>
+		<DndContext>
+			<div
+				ref={setNodeRef}
+				key={`${file}${rank}`}
+				className={`${isDark ? "bg-gray-400" : "bg-gray-100"} relative`}
+			>
+				{isSquareOnLeftEdge(file, "white") && (
+					<span
+						className={`absolute top-1 left-1 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{rank}</span>
+				)}
+
+				{isSquareOnBottomEdge(rank, "white") && (
+					<span
+						className={`absolute right-1 bottom-0.5 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{file}</span>
+				)}
+
+				{Object.entries(guesses[currentGuess].guess).map(([coordinate, squareInfo]) => {
+					if (!squareInfo) return null;
+					if (coordinate !== `${file}${rank}`) return null;
+
+					const color = squareInfo.color;
+					const piece = squareInfo.piece;
+
+					const pieceIcon = getPieceIcon(color, piece);
+					const key = `${coordinate} ${color} ${piece}`
+
+					return (
+						<img src={pieceIcon} key={key} alt={key} />
+					)
+				})}
+			</div>
+		</DndContext>
 	)
 }
 
