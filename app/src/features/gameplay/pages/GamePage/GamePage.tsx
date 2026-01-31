c49cba520a6266fbba8bf0f07d8f0f84cc33d0bb
Teddi <teddikao@gmail.com>
2026-01-31 17:47:46 +0800

    Ensure that piece setup menu and chessboard grid are under the same dnd context

--- app/src/features/gameplay/pages/GamePage/GamePage.tsx
+++ app/src/features/gameplay/pages/GamePage/GamePage.tsx
@@ -4,16 +4,28 @@ import PieceSetupMenu from "@/features/gameplay/pages/GamePage/components/PieceS
 import ChessboardGrid from "@/features/gameplay/pages/GamePage/components/ChessboardGrid/ChessboardGrid";
 import ActionsMenu from "@/features/gameplay/pages/GamePage/components/ActionsMenu";
 import CheckAnswerButton from "@/features/gameplay/pages/GamePage/components/CheckAnswerButton";
+import {DndContext, type DragEndEvent} from "@dnd-kit/core";
 
 function GamePage() {
+	function handleDragEnd(event: DragEndEvent) {
+		if (event.over) {
+			console.log(event.active.id);
+			console.log(event.over.id);
+		}
+	}
+
 	return (
 		<div className="flex flex-col items-center gap-4">
 			<Logo />
 			<GuessNavigator />
 
 			<div className="grid grid-cols-3 w-full justify-center gap-4">
-				<PieceSetupMenu />
-				<ChessboardGrid />
+				<DndContext onDragEnd={handleDragEnd}>
+					<PieceSetupMenu />
+					<ChessboardGrid />
+				</DndContext>
+
+
 				<ActionsMenu />
 			</div>
 
