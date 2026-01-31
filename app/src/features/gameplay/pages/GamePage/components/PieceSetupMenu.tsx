c48162ca5428d5c037d7f7ba99609a62567ca556
Teddi <teddikao@gmail.com>
2026-01-31 16:13:27 +0800

    Surrond piece setup menu in Dnd context

--- app/src/features/gameplay/pages/GamePage/components/PieceSetupMenu.tsx
+++ app/src/features/gameplay/pages/GamePage/components/PieceSetupMenu.tsx
@@ -1,26 +1,29 @@
 import {blackPieceIcons, whitePieceIcons} from "@/features/gameplay/constants/pieceIcons";
+import {DndContext} from "@dnd-kit/core";
 
 function PieceSetupMenu() {
 	return (
-		<div className="flex flex-row justify-end items-center">
-			<div className="flex flex-row w-max p-1 rounded-md shadow-md shadow-gray-400">
-				<div className="flex flex-col">
-					{Object.entries(whitePieceIcons).map(([abbreviation, icon]) => (
-						<button key={abbreviation} type="button" className="hover:bg-gray-400 rounded-md">
-							<img className="w-12 h-12" src={icon} alt={abbreviation} />
-						</button>
-					))}
-				</div>
+		<DndContext>
+			<div className="flex flex-row justify-end items-center">
+				<div className="flex flex-row w-max p-1 rounded-md shadow-md shadow-gray-400">
+					<div className="flex flex-col">
+						{Object.entries(whitePieceIcons).map(([abbreviation, icon]) => (
+							<button key={abbreviation} type="button" className="hover:bg-gray-400 rounded-md">
+								<img className="w-12 h-12" src={icon} alt={abbreviation}/>
+							</button>
+						))}
+					</div>
 
-				<div className="flex flex-col">
-					{Object.entries(blackPieceIcons).map(([abbreviation, icon]) => (
-						<button type="button" key={abbreviation} className="hover:bg-gray-400 rounded-md">
-							<img className="w-12 h-12" src={icon} alt={abbreviation} />
-						</button>
-					))}
+					<div className="flex flex-col">
+						{Object.entries(blackPieceIcons).map(([abbreviation, icon]) => (
+							<button type="button" key={abbreviation} className="hover:bg-gray-400 rounded-md">
+								<img className="w-12 h-12" src={icon} alt={abbreviation}/>
+							</button>
+						))}
+					</div>
 				</div>
 			</div>
-		</div>
+		</DndContext>
 	)
 }
 
