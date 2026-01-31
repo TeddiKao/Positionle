e74068270c7e99a2f65cc61169185b7a4f6a8bea
Teddi <teddikao@gmail.com>
2026-01-31 16:40:21 +0800

    Make piece icon a draggable component

--- app/src/features/gameplay/pages/GamePage/components/PieceSetupMenu/components/PieceForSetup.tsx
+++ app/src/features/gameplay/pages/GamePage/components/PieceSetupMenu/components/PieceForSetup.tsx
@@ -1,11 +1,21 @@
+import {useDraggable} from "@dnd-kit/core";
+
 type PieceForSetupProps = {
 	abbreviation: string,
 	icon: string,
 }
 
 function PieceForSetup({ abbreviation, icon }: PieceForSetupProps) {
+	const { attributes, listeners, setNodeRef, transform } = useDraggable({
+		id: abbreviation
+	});
+
+	const style = transform ? {
+		transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+	} : undefined;
+
 	return (
-		<button type="button" key={abbreviation} className="hover:bg-gray-400 rounded-md">
+		<button style={style} ref={setNodeRef} type="button" key={abbreviation} {...listeners} {...attributes} className="hover:bg-gray-400 rounded-md">
 			<img className="w-12 h-12" src={icon} alt={abbreviation}/>
 		</button>
 	)
