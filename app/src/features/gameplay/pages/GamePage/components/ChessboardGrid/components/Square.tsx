29115e5cc3420dcf16eb5d243718945fbcf7ac01
Teddi <teddikao@gmail.com>
2026-01-31 16:27:35 +0800

    Make square component a droppable component

--- app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square.tsx
+++ app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square.tsx
@@ -1,6 +1,7 @@
 import type {ChessPiece, PieceColor} from "@/features/gameplay/types/chess";
 import type {SquareCoordinate} from "@/features/gameplay/types/coordinates";
 import {getPieceIcon} from "@/features/gameplay/utils/pieceIconDetection";
+import {useDroppable} from "@dnd-kit/core";
 
 type SquareProps = {
 	color: PieceColor,
@@ -8,12 +9,18 @@ type SquareProps = {
 	coordinate: SquareCoordinate
 }
 
-function Square({ color, piece, coordinate }: SquareProps) {
+function Square({color, piece, coordinate}: SquareProps) {
+	const {setNodeRef} = useDroppable({
+		id: coordinate
+	});
+
 	const pieceIcon = getPieceIcon(color, piece);
 
 	return (
-		<img key={`${coordinate} ${color} ${piece}`} src={pieceIcon}
-			 alt={`${color} ${piece}`}/>
+		<div ref={setNodeRef}>
+			<img key={`${coordinate} ${color} ${piece}`} src={pieceIcon}
+								   alt={`${color} ${piece}`}/>
+		</div>
 	)
 }
 
