053c781aa033da5771f8c3f2da6be5a33b07dd46
Teddi <teddikao@gmail.com>
2026-01-31 16:30:59 +0800

    Move Dnd context to square component instead of whole chessboard grid

--- app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/ChessboardGrid.tsx
+++ app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/ChessboardGrid.tsx
@@ -1,8 +1,8 @@
 import {isSquareOnBottomEdge, isSquareOnLeftEdge} from "@/features/gameplay/utils/edgeDetection";
 import useGuessesStore from "@/features/gameplay/stores/guesses";
-import {DndContext} from "@dnd-kit/core";
 import Square from "@/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square";
 import type {SquareCoordinate} from "@/features/gameplay/types/coordinates";
+import {DndContext} from "@dnd-kit/core";
 
 function ChessboardGrid() {
 	const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
@@ -11,41 +11,42 @@ function ChessboardGrid() {
 	const {currentGuess, guesses} = useGuessesStore();
 
 	return (
-		<DndContext>
-			<div className="grid grid-cols-8 w-full aspect-square shadow-lg shadow-gray-600">
-				{ranks.map((rank) => (
-					files.map((file, fileIndex) => {
-						const isDark = (rank + fileIndex) % 2 === 1;
+		<div className="grid grid-cols-8 w-full aspect-square shadow-lg shadow-gray-600">
+			{ranks.map((rank) => (
+				files.map((file, fileIndex) => {
+					const isDark = (rank + fileIndex) % 2 === 1;
 
-						return (
-							<div
-								key={`${file}${rank}`}
-								className={`${isDark ? "bg-gray-400" : "bg-gray-100"} relative`}
-							>
-								{isSquareOnLeftEdge(file, "white") && (
-									<span
-										className={`absolute top-1 left-1 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{rank}</span>
-								)}
+					return (
+						<div
+							key={`${file}${rank}`}
+							className={`${isDark ? "bg-gray-400" : "bg-gray-100"} relative`}
+						>
+							{isSquareOnLeftEdge(file, "white") && (
+								<span
+									className={`absolute top-1 left-1 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{rank}</span>
+							)}
 
-								{isSquareOnBottomEdge(rank, "white") && (
-									<span
-										className={`absolute right-1 bottom-0.5 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{file}</span>
-								)}
+							{isSquareOnBottomEdge(rank, "white") && (
+								<span
+									className={`absolute right-1 bottom-0.5 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{file}</span>
+							)}
 
-								{Object.entries(guesses[currentGuess].guess).map(([coordinate, squareInfo]) => {
-									if (!squareInfo) return null;
-									if (coordinate !== `${file}${rank}`) return null;
+							{Object.entries(guesses[currentGuess].guess).map(([coordinate, squareInfo]) => {
+								if (!squareInfo) return null;
+								if (coordinate !== `${file}${rank}`) return null;
 
-									return <Square color={squareInfo.color} piece={squareInfo.piece}
-												   coordinate={coordinate as SquareCoordinate}
-												   key={`${coordinate} ${squareInfo.color} ${squareInfo.piece}`}/>
-								})}
-							</div>
-						)
-					})
-				))}
-			</div>
-		</DndContext>
+								return (
+									<DndContext>
+										<Square color={squareInfo.color} piece={squareInfo.piece}
+												  coordinate={coordinate as SquareCoordinate}
+												  key={`${coordinate} ${squareInfo.color} ${squareInfo.piece}`}/>
+									</DndContext>)
+							})}
+						</div>
+					)
+				})
+			))}
+		</div>
 	)
 }
 
