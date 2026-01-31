f22cf2df2be21017ba22d355df3175bd0d4b61e0
Teddi <teddikao@gmail.com>
2026-01-31 17:01:19 +0800

    Move Dnd context to appropriate components

--- app/src/features/gameplay/pages/GamePage/components/PieceSetupMenu/PieceSetupMenu.tsx
+++ app/src/features/gameplay/pages/GamePage/components/PieceSetupMenu/PieceSetupMenu.tsx
@@ -1,26 +1,34 @@
 import {blackPieceIcons, whitePieceIcons} from "@/features/gameplay/constants/pieceIcons";
-import {DndContext} from "@dnd-kit/core";
+import {DndContext, type DragEndEvent} from "@dnd-kit/core";
 import PieceForSetup from "@/features/gameplay/pages/GamePage/components/PieceSetupMenu/components/PieceForSetup";
 
 function PieceSetupMenu() {
+	function handleDragEnd(event: DragEndEvent) {
+		console.log("Drag ended")
+
+		if (event.over) {
+			console.log(event.over.id);
+		}
+	}
+
 	return (
 		<div className="flex flex-row justify-end items-center">
 			<div className="flex flex-row w-max p-1 rounded-md shadow-md shadow-gray-400">
 				<div className="flex flex-col">
-					<DndContext>
-						{Object.entries(whitePieceIcons).map(([abbreviation, icon]) => (
+					{Object.entries(whitePieceIcons).map(([abbreviation, icon]) => (
+						<DndContext key={abbreviation} onDragEnd={handleDragEnd}>
 							<PieceForSetup abbreviation={abbreviation} icon={icon} />
-						))}
-					</DndContext>
+						</DndContext>
+					))}
 				</div>
 
 				<div className="flex flex-col">
-					<DndContext>
-						{Object.entries(blackPieceIcons).map(([abbreviation, icon]) => (
+					{Object.entries(blackPieceIcons).map(([abbreviation, icon]) => (
+						<DndContext key={abbreviation} onDragEnd={handleDragEnd}>
 							<PieceForSetup abbreviation={abbreviation} icon={icon} />
-						))}
-					</DndContext>
-				</div>
+						</DndContext>
+					))}
+			</div>
 			</div>
 		</div>
 	)
