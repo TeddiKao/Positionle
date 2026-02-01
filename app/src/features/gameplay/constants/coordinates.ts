2339db258bcae30460a845e1737efbcdcaab81ff
Teddi <teddikao@gmail.com>
2026-02-01 10:17:21 +0800

    Remove uneccessary file and rank types and instead import them from separate file

--- app/src/features/gameplay/constants/coordinates.ts
+++ app/src/features/gameplay/constants/coordinates.ts
@@ -1,5 +1,4 @@
-type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-type File = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
+import type {File, Rank} from "@/features/gameplay/types/coordinates";
 
 const ranks: Rank[] = [8, 7, 6, 5, 4, 3, 2, 1];
 const files: File[] = ["a", "b", "c", "d", "e", "f", "g", "h"];
