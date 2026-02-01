6efa3587af4e5dcc3e7ce5df086ffd05d55b3b20
Teddi <teddikao@gmail.com>
2026-02-01 16:01:05 +0800

    Create function to check for pieces that are not in the game

--- app/src/features/gameplay/utils/answerCheck.ts
+++ app/src/features/gameplay/utils/answerCheck.ts
@@ -1,10 +1,30 @@
 import type {BoardRepresentation} from "@/features/gameplay/types/chess";
 import type {SquareCoordinate} from "@/features/gameplay/types/coordinates";
 import type {GuessResult} from "@/features/gameplay/types/guesses";
+import type {PieceAbbreviation} from "@/features/gameplay/types/abbreviations";
+import {colorAbbreviations, pieceAbbreviations} from "@/features/gameplay/constants/abbreviations";
+import {pieceCounts} from "@/features/gameplay/constants/answerCheck";
 
-function checkAnswer(correctPosition: BoardRepresentation, submittedPosition: BoardRepresentation) {
-	const guessResult: GuessResult = {}
+function getPieceCounts(position: BoardRepresentation) {
+	const positionPieceCounts = structuredClone(pieceCounts);
 
+	Object.entries(position).forEach(([, squareInfo]) => {
+		const color = squareInfo.color;
+		const piece = squareInfo.piece;
+
+		const abbreviation = `${colorAbbreviations[color]}${pieceAbbreviations[piece]}`;
+
+		if (pieceCounts[abbreviation as PieceAbbreviation] === undefined) {
+			pieceCounts[abbreviation as PieceAbbreviation] = 1;
+		} else {
+			pieceCounts[abbreviation as PieceAbbreviation] += 1;
+		}
+	});
+
+	return positionPieceCounts;
+}
+
+function detectCorrectPlacements(correctPosition: BoardRepresentation, submittedPosition: BoardRepresentation, guessResult: GuessResult) {
 	Object.entries(submittedPosition).forEach(([squareCoordinate, submittedSquareInfo]) => {
 		const correctSquareInfo = correctPosition[squareCoordinate as SquareCoordinate];
 		if (!correctSquareInfo) return;
@@ -17,6 +37,33 @@ function checkAnswer(correctPosition: BoardRepresentation, submittedPosition: Bo
 			taxiDistance: null
 		}
 	})
+
+}
+
+function detectPlacementsNotInGame(correctPosition: BoardRepresentation, submittedPosition: BoardRepresentation, guessResult: GuessResult) {
+	const correctPositionPieceCounts = getPieceCounts(correctPosition);
+	const submittedPositionPieceCounts = structuredClone(pieceCounts);
+
+	Object.entries(submittedPosition).forEach(([squareCoordinate, submittedSquareInfo]) => {
+		const submittedPiece = submittedSquareInfo.piece;
+		const submittedPieceColor = submittedSquareInfo.color;
+
+		const abbreviation = `${colorAbbreviations[submittedPieceColor]}${pieceAbbreviations[submittedPiece]}`;
+
+		if (correctPositionPieceCounts[abbreviation as PieceAbbreviation] <= submittedPositionPieceCounts[abbreviation as PieceAbbreviation]) {
+			guessResult[squareCoordinate as SquareCoordinate] = {
+				resultType: "notInGame",
+				taxiDistance: null
+			}
+		}
+	})
+}
+
+function checkAnswer(correctPosition: BoardRepresentation, submittedPosition: BoardRepresentation) {
+	const guessResult: GuessResult = {}
+
+	detectCorrectPlacements(correctPosition, submittedPosition, guessResult);
+	detectPlacementsNotInGame(correctPosition, submittedPosition, guessResult);
 
 	return guessResult;
 }
