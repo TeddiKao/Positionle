c630f72d07405538e664a966d9927794f0ac526a
Teddi <teddikao@gmail.com>
2026-02-01 10:13:52 +0800

    Flip ranks array

--- app/src/features/gameplay/constants/coordinates.ts
+++ app/src/features/gameplay/constants/coordinates.ts
@@ -1,7 +1,7 @@
 type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 type File = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
 
-const ranks: Rank[] = [1, 2, 3, 4, 5, 6, 7, 8];
+const ranks: Rank[] = [8, 7, 6, 5, 4, 3, 2, 1];
 const files: File[] = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 export { ranks, files }
