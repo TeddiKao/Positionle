f54bb273af878c9d690cd0a5f48ce67a2d89ea59
Teddi <teddikao@gmail.com>
2026-02-01 13:24:54 +0800

    Submit guess when button is clicked

--- app/src/features/gameplay/pages/GamePage/components/CheckAnswerButton.tsx
+++ app/src/features/gameplay/pages/GamePage/components/CheckAnswerButton.tsx
@@ -1,8 +1,15 @@
 import {Button} from "@/components/ui/button";
+import useGuessesStore from "@/features/gameplay/stores/guesses";
 
 function CheckAnswerButton() {
+	const { submitGuess } = useGuessesStore();
+
+	function handleAnswerSubmission() {
+		submitGuess();
+	}
+
 	return (
-		<Button type="button" className="w-full max-w-md hover:opacity-90">Check</Button>
+		<Button onClick={handleAnswerSubmission} type="button" className="w-full max-w-md hover:opacity-90">Check</Button>
 	)
 }
 
