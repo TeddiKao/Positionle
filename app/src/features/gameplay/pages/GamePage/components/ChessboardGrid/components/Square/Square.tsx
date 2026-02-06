5fe753bb070045523b8c75ae0ed44e1ba50b014f
Teddi <teddikao@gmail.com>
2026-02-06 12:18:53 +0800

    Add distance display logic in squares

--- app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square/Square.tsx
+++ app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square/Square.tsx
@@ -1,26 +1,35 @@
-import {useDroppable} from "@dnd-kit/core";
-import {isSquareOnBottomEdge, isSquareOnLeftEdge} from "@/features/gameplay/utils/edgeDetection";
+import { useDroppable } from "@dnd-kit/core";
+import {
+	isSquareOnBottomEdge,
+	isSquareOnLeftEdge,
+} from "@/features/gameplay/utils/edgeDetection";
 import useGuessesStore from "@/features/gameplay/stores/guesses";
-import PieceIcon
-	from "@/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square/components/PieceIcon";
-import type {File, Rank, SquareCoordinate} from "@/features/gameplay/types/coordinates";
-import {files} from "@/features/gameplay/constants/coordinates";
-import {clsx} from "clsx";
-import {getColorByDistance} from "@/features/gameplay/utils/colorComputation";
+import PieceIcon from "@/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square/components/PieceIcon";
+import type {
+	File,
+	Rank,
+	SquareCoordinate,
+} from "@/features/gameplay/types/coordinates";
+import { files } from "@/features/gameplay/constants/coordinates";
+import { clsx } from "clsx";
+import { getColorByDistance } from "@/features/gameplay/utils/colorComputation";
+import { useDistanceDisplayStore } from "@/features/gameplay/stores/distanceDisplay";
 
 type SquareProps = {
-	file: File,
-	rank: Rank,
-}
+	file: File;
+	rank: Rank;
+};
 
-function Square({file, rank}: SquareProps) {
-	const {setNodeRef} = useDroppable({
-		id: `${file}${rank}`
+function Square({ file, rank }: SquareProps) {
+	const { setNodeRef } = useDroppable({
+		id: `${file}${rank}`,
 	});
 
 	const { guesses, currentGuess } = useGuessesStore();
 	const guessResult = guesses[currentGuess].guessResult;
-	const squareResult = guessResult?.[`${file}${rank}`]
+	const squareResult = guessResult?.[`${file}${rank}`];
+
+	const { isShowingExactDistances } = useDistanceDisplayStore();
 
 	const fileIndex = files.indexOf(file);
 	const isDark = (rank + fileIndex) % 2 === 1;
@@ -28,30 +37,30 @@ function Square({file, rank}: SquareProps) {
 	function getColorClass() {
 		if (squareResult?.resultType === "correct") {
 			if (isDark) {
-				return "bg-green-700"
+				return "bg-green-700";
 			} else {
-				return "bg-green-600"
+				return "bg-green-600";
 			}
 		}
 
 		if (squareResult?.resultType === "wrongPosition") {
 			if (squareResult.taxiDistance === null) return;
 
-			return getColorByDistance(squareResult?.taxiDistance, isDark)
+			return getColorByDistance(squareResult?.taxiDistance, isDark);
 		}
 
 		if (squareResult?.resultType === "notInGame") {
 			if (isDark) {
-				return "bg-gray-700"
+				return "bg-gray-700";
 			} else {
-				return "bg-gray-600"
+				return "bg-gray-600";
 			}
 		}
 
 		if (isDark) {
 			return "bg-gray-400";
 		} else {
-			return "bg-gray-100"
+			return "bg-gray-100";
 		}
 	}
 
@@ -63,22 +72,47 @@ function Square({file, rank}: SquareProps) {
 		>
 			{isSquareOnLeftEdge(file, "white") && (
 				<span
-					className={`absolute top-1 left-1 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{rank}</span>
+					className={`absolute top-1 left-1 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}
+				>
+					{rank}
+				</span>
 			)}
 
 			{isSquareOnBottomEdge(rank, "white") && (
 				<span
-					className={`absolute right-1 bottom-0.5 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}>{file}</span>
+					className={`absolute right-1 bottom-0.5 text-xs font-bold ${isDark ? "text-gray-100" : "text-gray-400"}`}
+				>
+					{file}
+				</span>
 			)}
 
-			{Object.entries(guesses[currentGuess]?.guess ?? {}).map(([coordinate, squareInfo]) => {
-				if (!squareInfo) return null;
-				if (coordinate !== `${file}${rank}`) return null;
+			{isShowingExactDistances && (
+				<span
+					className={clsx(
+						"relative w-full h-full",
+						isDark ? "text-white" : "text-gray-950",
+					)}
+				>
+					{squareResult?.taxiDistance}
+				</span>
+			)}
 
-				return <PieceIcon key={coordinate} squareInfo={squareInfo} coordinate={coordinate as SquareCoordinate} />
-			})}
+			{Object.entries(guesses[currentGuess]?.guess ?? {}).map(
+				([coordinate, squareInfo]) => {
+					if (!squareInfo) return null;
+					if (coordinate !== `${file}${rank}`) return null;
+
+					return (
+						<PieceIcon
+							key={coordinate}
+							squareInfo={squareInfo}
+							coordinate={coordinate as SquareCoordinate}
+						/>
+					);
+				},
+			)}
 		</div>
-	)
+	);
 }
 
 export default Square;
