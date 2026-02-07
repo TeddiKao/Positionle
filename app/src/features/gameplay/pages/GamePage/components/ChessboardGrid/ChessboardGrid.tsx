266331d1a98f2e7e482efd2a7b57bf3bb59a8c94
Teddi <teddikao@gmail.com>
2026-02-07 13:05:49 +0800

    Add chessboard generation logic for flipped boards

--- app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/ChessboardGrid.tsx
+++ app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/ChessboardGrid.tsx
@@ -1,16 +1,61 @@
 import Square from "@/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square/Square";
-import {files, ranks} from "@/features/gameplay/constants/coordinates";
+import { files, ranks } from "@/features/gameplay/constants/coordinates";
+import useGuessesStore from "@/features/gameplay/stores/guesses";
 
 function ChessboardGrid() {
-	return (
+	const { guesses, currentGuess } = useGuessesStore();
+
+	const orientation = guesses[currentGuess].orientation;
+
+	return orientation === "white" ? (
 		<div className="grid grid-cols-8 w-full aspect-square shadow-lg shadow-gray-600">
-			{ranks.map((rank) => (
+			{ranks.map((rank) =>
 				files.map((file) => {
-					return <Square key={`${file}${rank}`} file={file as "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h"} rank={rank as 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8} />
-				})
-			))}
+					return (
+						<Square
+							key={`${file}${rank}`}
+							file={
+								file as
+									| "a"
+									| "b"
+									| "c"
+									| "d"
+									| "e"
+									| "f"
+									| "g"
+									| "h"
+							}
+							rank={rank as 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8}
+						/>
+					);
+				}),
+			)}
 		</div>
-	)
+	) : (
+		<div className="grid grid-cols-8 w-full aspect-square shadow-lg shadow-gray-600">
+			{ranks.reverse().map((rank) =>
+				files.reverse().map((file) => {
+					return (
+						<Square
+							key={`${file}${rank}`}
+							file={
+								file as
+									| "a"
+									| "b"
+									| "c"
+									| "d"
+									| "e"
+									| "f"
+									| "g"
+									| "h"
+							}
+							rank={rank as 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8}
+						/>
+					);
+				}),
+			)}
+		</div>
+	);
 }
 
 export default ChessboardGrid;
