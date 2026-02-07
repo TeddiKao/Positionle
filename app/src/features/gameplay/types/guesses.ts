18f4f5c19d491e80ae881f6e20b8738ab9a1f497
Teddi <teddikao@gmail.com>
2026-02-07 12:59:24 +0800

    Modify default guess info to include orientation property

--- app/src/features/gameplay/types/guesses.ts
+++ app/src/features/gameplay/types/guesses.ts
@@ -1,4 +1,7 @@
-import type {BoardRepresentation} from "@/features/gameplay/types/chess";
+import type {
+	BoardRepresentation,
+	PieceColor,
+} from "@/features/gameplay/types/chess";
 import type {SquareCoordinate} from "@/features/gameplay/types/coordinates";
 
 type GuessNumbers = 1 | 2 | 3 | 4 | 5 | 6;
@@ -13,7 +16,8 @@ type GuessResult = Partial<Record<SquareCoordinate, SquareResult>>
 type GuessInfo = {
 	guess: BoardRepresentation;
 	isSubmitted: boolean;
-	guessResult: GuessResult | null
+	guessResult: GuessResult | null;
+	orientation: PieceColor;
 }
 
 export type { GuessInfo, GuessNumbers, GuessResult };
