becfaf363893f1ab4482b7ee719d01d57b93ac52
Teddi <teddikao@gmail.com>
2026-02-07 17:12:48 +0800

    Revert touch manipulation change and use touch none to allow touchscreen dragging

--- app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square/components/PieceIcon.tsx
+++ app/src/features/gameplay/pages/GamePage/components/ChessboardGrid/components/Square/components/PieceIcon.tsx
@@ -31,7 +31,7 @@ function PieceIcon({ squareInfo, coordinate }: PieceIconProps) {
 			ref={setNodeRef}
 			{...attributes}
 			{...listeners}
-			className="absolute z-40 touch-manipulation"
+			className="absolute z-40 touch-none pointer-events-auto"
 			src={pieceIcon}
 			key={key}
 			alt={key}
