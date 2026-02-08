b0e5c18091e9d992fa2b8b79d63755909db2be4e
Teddi <teddikao@gmail.com>
2026-02-09 07:44:26 +0800

    Update test position accordingly

--- app/src/dev/testPositions.ts
+++ app/src/dev/testPositions.ts
@@ -1,17 +1,22 @@
-import type {BoardRepresentation} from "@/features/gameplay/types/chess";
+import type { CorrectPositionInfo } from "@/features/gameplay/types/guesses";
 
-const dualKingsideCastlingTest: BoardRepresentation = {
-	f1: { color: "white", piece: "rook" },
-	g1: { color: "white", piece: "king" },
-	f2: { color: "white", piece: "pawn" },
-	g2: { color: "white", piece: "pawn" },
-	h2: { color: "white", piece: "pawn" },
+const dualKingsideCastlingTest: CorrectPositionInfo = {
+	correctPosition: {
+		f1: { color: "white", piece: "rook" },
+		g1: { color: "white", piece: "king" },
+		f2: { color: "white", piece: "pawn" },
+		g2: { color: "white", piece: "pawn" },
+		h2: { color: "white", piece: "pawn" },
 
-	f7: { color: "black", piece: "pawn" },
-	g7: { color: "black", piece: "pawn" },
-	h7: { color: "black", piece: "pawn" },
-	f8: { color: "black", piece: "rook" },
-	g8: { color: "black", piece: "king" },
-}
+		f7: { color: "black", piece: "pawn" },
+		g7: { color: "black", piece: "pawn" },
+		h7: { color: "black", piece: "pawn" },
+		f8: { color: "black", piece: "rook" },
+		g8: { color: "black", piece: "king" },
+	},
+	source: "Unknown",
+	whitePlayer: "NN",
+	blackPlayer: "NN",
+};
 
-export { dualKingsideCastlingTest }
+export { dualKingsideCastlingTest };
