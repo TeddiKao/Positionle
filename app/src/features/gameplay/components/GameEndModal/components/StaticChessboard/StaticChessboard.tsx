7aa401c8e33e9eec585b0dc7842760493a13afb0
Teddi <teddikao@gmail.com>
2026-02-08 18:33:07 +0800

    Create static chessboard component to display in game end modal

---
+++ app/src/features/gameplay/components/GameEndModal/components/StaticChessboard/StaticChessboard.tsx
@@ -0,0 +1,29 @@
+import type { BoardRepresentation } from "@/features/gameplay/types/chess";
+import { files, ranks } from "@/features/gameplay/constants/coordinates";
+import StaticSquare from "@/features/gameplay/components/GameEndModal/components/StaticChessboard/components/StaticSquare";
+
+type StaticChessboardProps = {
+	boardRepresentation: BoardRepresentation;
+};
+
+function StaticChessboard({ boardRepresentation }: StaticChessboardProps) {
+	return (
+		<div className="grid grid-cols-8 w-full aspect-square shadow-lg shadow-gray-600">
+			{ranks.map((rank) =>
+				files.map((file) => {
+					return (
+						<StaticSquare
+							squareInfo={
+								boardRepresentation[`${file}${rank}`] ?? null
+							}
+							file={file}
+							rank={rank}
+						/>
+					);
+				}),
+			)}
+		</div>
+	);
+}
+
+export default StaticChessboard;
