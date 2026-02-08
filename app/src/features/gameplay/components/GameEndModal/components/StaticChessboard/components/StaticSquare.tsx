a01aa65a147dc69934d93e556d1e65578b8d6e59
Teddi <teddikao@gmail.com>
2026-02-08 18:28:53 +0800

    Create static square component to display once game ends

---
+++ app/src/features/gameplay/components/GameEndModal/components/StaticChessboard/components/StaticSquare.tsx
@@ -0,0 +1,23 @@
+import type { SquareInfo } from "@/features/gameplay/types/chess";
+import type { File, Rank } from "@/features/gameplay/types/coordinates";
+import { getPieceIcon } from "@/features/gameplay/utils/pieceIconDetection";
+
+type StaticSquareProps = {
+	squareInfo: SquareInfo;
+	file: File;
+	rank: Rank;
+};
+
+function StaticSquare({ squareInfo, file, rank }: StaticSquareProps) {
+	if (!squareInfo.color) return null;
+	if (!squareInfo.piece) return null;
+
+	return (
+		<img
+			alt={`${file}${rank}`}
+			src={getPieceIcon(squareInfo.color, squareInfo.piece)}
+		/>
+	);
+}
+
+export default StaticSquare;
