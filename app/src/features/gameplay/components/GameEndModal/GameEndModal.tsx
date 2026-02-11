30286ced767fa75477ac380b76d2cc2a72470876
Teddi <teddikao@gmail.com>
2026-02-11 10:52:47 +0800

    Add play again button

--- app/src/features/gameplay/components/GameEndModal/GameEndModal.tsx
+++ app/src/features/gameplay/components/GameEndModal/GameEndModal.tsx
@@ -2,12 +2,14 @@ import {
 	Dialog,
 	DialogContent,
 	DialogDescription,
+	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 } from "@/components/ui/dialog";
 import StaticChessboard from "@/features/gameplay/components/GameEndModal/components/StaticChessboard/StaticChessboard";
 import useGuessesStore from "@/features/gameplay/stores/guesses";
 import useGameEndModalStore from "@/features/gameplay/stores/gameEndModal";
+import { Button } from "@/components/ui/button";
 
 function GameEndModal() {
 	const { correctPositionInfo, hasCorrectlyGuessed } = useGuessesStore();
@@ -57,6 +59,12 @@ function GameEndModal() {
 						</p>
 					</div>
 				</div>
+
+				<DialogFooter>
+					<Button className="w-full hover:opacity-90">
+						Play again
+					</Button>
+				</DialogFooter>
 			</DialogContent>
 		</Dialog>
 	);
