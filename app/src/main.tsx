a0a5fcd202dea76cc5258a79694f82d67b23de48
Teddi <teddikao@gmail.com>
2026-02-14 15:27:51 +0800

    Use posthog client instead of API key

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,20 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router";
+
+import { posthog } from "posthog-js";
 import { PostHogProvider } from "@posthog/react";
 
-const options = {
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
 	api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
 	defaults: "2026-01-30",
-} as const;
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<PostHogProvider
-				apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-				options={options}
-			>
+			<PostHogProvider client={posthog}>
 				<App />
 			</PostHogProvider>
 		</BrowserRouter>
