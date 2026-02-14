024d5c0170cad5242654fe5fba00442c7a5166d7
Teddi <teddikao@gmail.com>
2026-02-14 14:05:18 +0800

    Add PostHog configuration to project

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,11 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router";
+import { PostHogProvider } from "@posthog/react";
+
+const options = {
+	api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+	defaults: "2026-01-30",
+} as const;
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+			<PostHogProvider
+				apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+				options={options}
+			>
+				<App />
+			</PostHogProvider>
 		</BrowserRouter>
 	</StrictMode>,
 );
