b082a24b5ffb113572f0650b90b2204f18ae1d7e
Teddi <teddikao@gmail.com>
2026-02-16 16:29:09 +0800

    Create type and placeholder function for annotation toolbar store

---
+++ app/src/features/gameplay/stores/annotationToolbar.ts
@@ -0,0 +1,12 @@
+import type { CanvasPath } from "react-sketch-canvas";
+import { create } from "zustand";
+
+type AnnotationToolbarStore = {
+	updateAnnotations: (annotations: CanvasPath[]) => void;
+	activateAnnotationEraser: () => void;
+	deactivateAnnotationEraser: () => void;
+};
+
+const useAnnotationToolbarStore = create((set) => {});
+
+export default useAnnotationToolbarStore;
