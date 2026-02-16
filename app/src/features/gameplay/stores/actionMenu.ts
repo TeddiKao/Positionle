c845a56210f5646f17c3bd0852b60b2fa63ba774
Teddi <teddikao@gmail.com>
2026-02-16 17:14:38 +0800

    Update type definitions of action menu store

--- app/src/features/gameplay/stores/actionMenu.ts
+++ app/src/features/gameplay/stores/actionMenu.ts
@@ -1,19 +1,24 @@
 import { create } from "zustand";
+import type { GuessNumbers } from "@/features/gameplay/types/guesses";
 
 type ActionMenuStore = {
-	clearGuess: () => void;
-	flipBoard: () => void;
+	clearGuess: (guessNumber: GuessNumbers) => void;
+	flipBoard: (guessNumber: GuessNumbers) => void;
 
-	showExactDistances: () => void;
-	hideExactDistances: () => void;
+	showExactDistances: (guessNumber: GuessNumbers) => void;
+	hideExactDistances: (guessNumber: GuessNumbers) => void;
 
+	isEraserModeActive: boolean;
 	activateEraserMode: () => void;
 	deactivateEraserMode: () => void;
 
+	isPenActive: boolean;
 	activatePen: () => void;
 	deactivatePen: () => void;
 };
 
-const useActionMenuStore = create((set) => {});
+const useActionMenuStore = create<ActionMenuStore>((set) => ({
+
+});
 
 export default useActionMenuStore;
