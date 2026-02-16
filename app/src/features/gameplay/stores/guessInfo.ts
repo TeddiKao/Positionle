70efee8efbbb16b53a4a9cf424c35fbf5849ea56
Teddi <teddikao@gmail.com>
2026-02-16 17:03:53 +0800

    Add updating of board in type definition

--- app/src/features/gameplay/stores/guessInfo.ts
+++ app/src/features/gameplay/stores/guessInfo.ts
@@ -4,15 +4,32 @@ import type {
 } from "@/features/gameplay/types/guesses";
 import { create } from "zustand";
 
+import type {
+	BoardRepresentation,
+	PieceColor,
+} from "@/features/gameplay/types/chess";
+import type { CanvasPath } from "react-sketch-canvas";
+
 type GuessInfoStore = {
 	guesses: Record<GuessNumbers, GuessInfo>;
 
-	updateAnnotationsForGuess: (guess: GuessNumbers) => void;
+	updateBoardForGuess: (
+		guess: GuessNumbers,
+		board: BoardRepresentation,
+	) => void;
+
+	updateAnnotationsForGuess: (
+		guess: GuessNumbers,
+		annotations: CanvasPath[],
+	) => void;
 
 	showExactDistancesForGuess: (guess: GuessNumbers) => void;
 	hideExactDistancesForGuess: (guess: GuessNumbers) => void;
 
-	updateOrientationForGuess: (guess: GuessNumbers) => void;
+	updateOrientationForGuess: (
+		guess: GuessNumbers,
+		orientation: PieceColor,
+	) => void;
 };
 
 const useGuessInfoStore = create((set) => ({}));
