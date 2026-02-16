934014f5748bcd04c22e769a48a00e0ca943cd8e
Teddi <teddikao@gmail.com>
2026-02-16 17:23:23 +0800

    Create functions for annotation toolbar store

--- app/src/features/gameplay/stores/annotationToolbar.ts
+++ app/src/features/gameplay/stores/annotationToolbar.ts
@@ -1,14 +1,32 @@
 import type { CanvasPath } from "react-sketch-canvas";
 import { create } from "zustand";
+import type { GuessNumbers } from "@/features/gameplay/types/guesses";
+import useGuessInfoStore from "@/features/gameplay/stores/guessInfo";
 
 type AnnotationToolbarStore = {
-	updateAnnotations: (annotations: CanvasPath[]) => void;
-
-	isAnnotationEraserActive: () => void;
+	isAnnotationEraserActive: boolean;
 	activateAnnotationEraser: () => void;
 	deactivateAnnotationEraser: () => void;
+
+	updateAnnotationsForGuess: (
+		guessNumber: GuessNumbers,
+		annotations: CanvasPath[],
+	) => void;
 };
 
-const useAnnotationToolbarStore = create((set) => {});
+const useAnnotationToolbarStore = create<AnnotationToolbarStore>((set) => ({
+	isAnnotationEraserActive: false,
+	activateAnnotationEraser: () => set({ isAnnotationEraserActive: true }),
+	deactivateAnnotationEraser: () => set({ isAnnotationEraserActive: false }),
+
+	updateAnnotationsForGuess: (
+		guessNumber: GuessNumbers,
+		annotations: CanvasPath[],
+	) => {
+		useGuessInfoStore
+			.getState()
+			.updateAnnotationsForGuess(guessNumber, annotations);
+	},
+}));
 
 export default useAnnotationToolbarStore;
