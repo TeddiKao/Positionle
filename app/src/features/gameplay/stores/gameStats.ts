d00fbb466cbd0b79307c543e2e1d0b51835e21b7
Teddi <teddikao@gmail.com>
2026-02-17 10:35:35 +0800

    Create store to track game stats

---
+++ app/src/features/gameplay/stores/gameStats.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+type GameStatsStore = {
+	gamesPlayed: number;
+	incrementGamesPlayed: () => void;
+
+	gamesWon: number;
+	incrementGamesWon: () => void;
+
+	resetGameStats: () => void;
+};
+
+const useGameStatsStore = create<GameStatsStore>()(
+	persist(
+		(set, _get, store) => ({
+			gamesPlayed: 0,
+			incrementGamesPlayed: () => {
+				set((state) => {
+					return { gamesPlayed: state.gamesPlayed + 1 };
+				});
+			},
+
+			gamesWon: 0,
+			incrementGamesWon: () => {
+				set((state) => {
+					return { gamesWon: state.gamesWon + 1 };
+				});
+			},
+
+			resetGameStats: () => set(store.getInitialState()),
+		}),
+		{ name: "gameStats", storage: createJSONStorage(() => localStorage) },
+	),
+);
+
+export default useGameStatsStore;
