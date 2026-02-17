1ec166f6f7c56f739c8e6a5bdbfbef3de1e3ed4b
Teddi <teddikao@gmail.com>
2026-02-17 11:25:04 +0800

    Add markup for stats title

--- app/src/features/gameplay/components/GameStatsModal.tsx
+++ app/src/features/gameplay/components/GameStatsModal.tsx
@@ -1,3 +1,22 @@
-function GameStatsModal() {}
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+
+function GameStatsModal() {
+	return (
+		<Dialog open={true}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle className="text-center text-base">
+						Stats
+					</DialogTitle>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	);
+}
 
 export default GameStatsModal;
