85f624dc4a235f2d7a5f55e36ebb7e3400e5af1a
Teddi <teddikao@gmail.com>
2026-02-17 11:45:55 +0800

    Create markup for game stats

--- app/src/features/gameplay/components/GameStatsModal.tsx
+++ app/src/features/gameplay/components/GameStatsModal.tsx
@@ -14,6 +14,38 @@ function GameStatsModal() {
 						Stats
 					</DialogTitle>
 				</DialogHeader>
+
+				<div className="flex flex-col gap-4">
+					<div className="grid grid-cols-4">
+						<div className="flex flex-col">
+							<span className="text-center font-bold text-2xl">
+								10
+							</span>
+							<span className="text-center">Games played</span>
+						</div>
+
+						<div className="flex flex-col">
+							<span className="text-center font-bold text-2xl">
+								80%
+							</span>
+							<span className="text-center">Win rate</span>
+						</div>
+
+						<div className="flex flex-col">
+							<span className="text-center font-bold text-2xl">
+								7
+							</span>
+							<span className="text-center">Current streak</span>
+						</div>
+
+						<div className="flex flex-col">
+							<span className="text-center font-bold text-2xl">
+								7
+							</span>
+							<span className="text-center">Highest streak</span>
+						</div>
+					</div>
+				</div>
 			</DialogContent>
 		</Dialog>
 	);
