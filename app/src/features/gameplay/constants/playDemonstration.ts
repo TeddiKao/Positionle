14c2f3be8314d0ab1005a7b96e2cd5012e54e0ce
Teddi <teddikao@gmail.com>
2026-02-18 11:05:41 +0800

    Create constants for additional demonstration positions

--- app/src/features/gameplay/constants/playDemonstration.ts
+++ app/src/features/gameplay/constants/playDemonstration.ts
@@ -2,11 +2,39 @@ import type { BoardRepresentation } from "@/features/gameplay/types/chess";
 import type { SquareCoordinate } from "@/features/gameplay/types/coordinates";
 
 const correctPositionDemonstration: BoardRepresentation = {
-	e4: { piece: "pawn", color: "white" }
-}
+	e4: { piece: "pawn", color: "white" },
+};
 
 const correctPositionColorClasses: Partial<Record<SquareCoordinate, string>> = {
 	e4: "bg-green-600",
 };
 
-export { correctPositionDemonstration, correctPositionColorClasses };
+const notInGameDemonstration: BoardRepresentation = {
+	d4: { piece: "knight", color: "white" },
+};
+
+const notInGameColorClasses: Partial<Record<SquareCoordinate, string>> = {
+	d4: "bg-gray-700",
+};
+
+const wrongLocationDemonstration: BoardRepresentation = {
+	a2: { piece: "pawn", color: "white" },
+};
+
+const wrongLocationColorClasses: Partial<Record<SquareCoordinate, string>> = {
+	a2: "bg-yellow-500",
+};
+
+const wrongLocationDistances: Partial<Record<SquareCoordinate, number>> = {
+	a2: 6,
+};
+
+export {
+	correctPositionDemonstration,
+	correctPositionColorClasses,
+	notInGameDemonstration,
+	notInGameColorClasses,
+	wrongLocationDemonstration,
+	wrongLocationColorClasses,
+	wrongLocationDistances,
+};
